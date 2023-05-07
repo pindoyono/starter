5480fd58197ee6228b1ea89024dc886a085ed0f3
pindoyono <pindoyonopatan@gmail.com>
2023-05-08 06:59:55 +0800

    dahsboard

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -1,3 +1,119 @@
-Data Dasboard
+{{-- <p>
+    Data Dasboard
+</p> --}}
 
 {{-- <button onclick="Livewire.emit('openModal', 'hello-world')">Open Modal</button> --}}
+
+<!-- component -->
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+<link rel="stylesheet"
+    href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+
+<div class="flex flex-wrap bg-grey-500 ">
+    <div class="w-full px-5 mt-4 mb-4 lg:w-6/12 xl:w-3/12">
+        <div class="relative flex flex-col min-w-0 mb-3 break-words bg-white rounded shadow-lg xl:mb-0">
+            <div class="flex-auto p-4">
+                <div class="flex flex-wrap">
+                    <div class="relative flex-1 flex-grow w-full max-w-full pr-4">
+                        <h5 class="text-xs font-bold uppercase text-blueGray-400"> Sekolah</h5>
+                        <span class="text-xl font-semibold text-blueGray-700">100</span>
+                    </div>
+                    <div class="relative flex-initial w-auto pl-4">
+                        <div
+                            class="inline-flex items-center justify-center w-12 h-12 p-3 text-center text-white bg-red-500 rounded-full shadow-lg">
+                            <i class="fas fa-chart-bar"></i>
+                        </div>
+                    </div>
+                </div>
+                <p class="mt-4 text-sm text-blueGray-400">
+                    <span class="mr-2 text-emerald-500"><i class="fas fa-arrow-up"></i> 100 </span>
+                    <span class="whitespace-nowrap"> Ruang Praktik </span>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="w-full px-5 mt-4 lg:w-6/12 xl:w-3/12">
+        <div class="relative flex flex-col min-w-0 mb-4 break-words bg-white rounded shadow-lg xl:mb-0">
+            <div class="flex-auto p-4">
+                <div class="flex flex-wrap">
+                    <div class="relative flex-1 flex-grow w-full max-w-full pr-4">
+                        <h5 class="text-xs font-bold uppercase text-blueGray-400">Pengguna</h5>
+                        </h5>
+                        <span class="text-xl font-semibold text-blueGray-700">100</span>
+                    </div>
+                    <div class="relative flex-initial w-auto pl-4">
+                        <div
+                            class="inline-flex items-center justify-center w-12 h-12 p-3 text-center text-white bg-pink-500 rounded-full shadow-lg">
+                            <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <p class="mt-4 text-sm text-blueGray-400">
+                    <span class="mr-2 text-red-500"><i class="fas fa-arrow-down"></i> 100</span>
+                    <span class="whitespace-nowrap"> Bursa Kerja Khusus </span>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="w-full px-5 mt-4 lg:w-6/12 xl:w-3/12">
+        <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white rounded shadow-lg xl:mb-0">
+            <div class="flex-auto p-4">
+                <div class="flex flex-wrap">
+                    <div class="relative flex-1 flex-grow w-full max-w-full pr-4">
+                        <h5 class="text-xs font-bold uppercase text-blueGray-400">Sales</h5>
+                        <span class="text-xl font-semibold text-blueGray-700">901</span>
+                    </div>
+                    <div class="relative flex-initial w-auto pl-4">
+                        <div
+                            class="inline-flex items-center justify-center w-12 h-12 p-3 text-center text-white rounded-full shadow-lg bg-lightBlue-500">
+                            <i class="fas fa-users"></i>
+                        </div>
+                    </div>
+                </div>
+                <p class="mt-4 text-sm text-blueGray-400">
+                    <span class="mr-2 text-red-500"><i class="fas fa-arrow-down"></i> 1,25% </span>
+                    <span class="whitespace-nowrap"> Since yesterday </span>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="w-full px-5 mt-4 lg:w-6/12 xl:w-3/12">
+        <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white rounded shadow-lg xl:mb-0">
+            <div class="flex-auto p-4">
+                <div class="flex flex-wrap">
+                    <div class="relative flex-1 flex-grow w-full max-w-full pr-4">
+                        <h5 class="text-xs font-bold uppercase text-blueGray-400">Performance</h5>
+                        <span class="text-xl font-semibold text-blueGray-700">51.02% </span>
+                    </div>
+                    <div class="relative flex-initial w-auto pl-4">
+                        <div
+                            class="inline-flex items-center justify-center w-12 h-12 p-3 text-center text-white rounded-full shadow-lg bg-emerald-500">
+                            <i class="fas fa-percent"></i>
+                        </div>
+                    </div>
+                </div>
+                <p class="mt-4 text-sm text-blueGray-400">
+                    <span class="mr-2 text-emerald-500"><i class="fas fa-arrow-up"></i> 12% </span>
+                    <span class="whitespace-nowrap"> Since last mounth </span>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- <footer class="relative pt-8 pb-6">
+    <div class="container px-4 mx-auto">
+        <div class="flex flex-wrap items-center justify-center md:justify-between">
+            <div class="w-full px-4 mx-auto text-center md:w-6/12">
+                <div class="py-1 text-sm font-semibold text-blueGray-500">
+                    Made with <a href="https://www.creative-tim.com/product/notus-js"
+                        class="text-blueGray-500 hover:text-gray-800" target="_blank">Notus JS</a> by <a
+                        href="https://www.creative-tim.com" class="text-blueGray-500 hover:text-blueGray-800"
+                        target="_blank"> Creative Tim</a>.
+                </div>
+            </div>
+        </div>
+    </div>
+</footer> --}}
