623da3c60e06b350d5b90147f02f19c03e93426b
pindoyono <pindoyonopatan@gmail.com>
2023-06-05 13:31:21 +0800

    ys

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @hasSection('title')
-        <title>@yield('title') - {{ config('app.name') }}</title>
+        <title>{{ config('app.name') }}</title>
     @else
         <title>{{ config('app.name') }}</title>
     @endif
