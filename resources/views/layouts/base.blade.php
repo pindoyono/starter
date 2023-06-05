fc69852003f82efec2ee889048cd81647dab727d
pindoyono <pindoyonopatan@gmail.com>
2023-06-05 13:30:08 +0800

    fav ico

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @hasSection('title')
 
-            <title>@yield('title') - {{ config('app.name') }}</title>
-        @else
-            <title>{{ config('app.name') }}</title>
-        @endif
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @hasSection('title')
+        <title>@yield('title') - {{ config('app.name') }}</title>
+    @else
+        <title>{{ config('app.name') }}</title>
+    @endif
 
-        <!-- Favicon -->
-		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
+    <!-- Favicon -->
+    {{-- <link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}"> --}}
+    <link rel="shortcut icon" href="{{ url(asset('favicon.png')) }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-        @livewireStyles
-        @livewireScripts
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-    </head>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @livewireStyles
+    @livewireScripts
 
-    <body>
-        @yield('body')
-    </body>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+<body>
+    @yield('body')
+</body>
+
 </html>
